cea2ca63293fc06bbf01a7a16182b950caceb98f
Shilpi <sheenu.anu@gmail.com>
2018-10-23 20:48:07 +0530

    Create UserInput.py

---
+++ UserInput.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+i = 4
+d = 4.0
+s = 'HackerRank '
+a = int(input())
+b = round(float(input()), 1)
+c = input()
+print(a+i)
+print(b+d)
+print(s+c)
